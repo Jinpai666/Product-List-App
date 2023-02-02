06581b912c1ea6e256bbd92cd95d457919b7f068
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-02-02 19:33:35 +0100

    first logic for adding items done

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,38 +1,100 @@
 import React from 'react';
 import './AddProduct.scss'
-import {Link} from "react-router-dom";
+import {Link, useNavigate } from "react-router-dom";
 import {useEffect, useState} from "react";
 
 function AddProduct(props) {
-    const [listItems, setListItems] = useState([]) ;
+    let itemsFromStorage = (JSON.parse(window.localStorage.getItem('list')));
+    console.log(itemsFromStorage)
+    const [name, setName] = useState('')
+    const [SKU, setSKU] = useState('')
+    const [price, setPrice] = useState('')
+    const [unique, setUnique] = useState('furniture')
+    const [uniqueText, setUniqueText] = useState('Dimensions')
 
-    useEffect(()=>{
-        window.localStorage.getItem('list') && setListItems (JSON.parse(window.localStorage.getItem('list')));
-        console.log('zmieniam')
-    },[]);
+    const navigate = useNavigate();
+
+    const handleSave = () => {
+        let newItem;
+        if (unique === 'furniture') {
+            newItem = {
+                SKU: SKU,
+                name: name,
+                price: price,
+                type: unique,
+                dimensions: 'dimensions'
+            }
+        }
+        if (unique === 'dvd') {
+            newItem = {
+                SKU: SKU,
+                name: name,
+                price: price,
+                type: unique,
+                Size: 'Size'
+
+            }
+        }
+        if (unique === 'book') {
+            newItem = {
+                SKU: SKU,
+                name: name,
+                price: price,
+                type: unique,
+                weight: 'weight'
+            }
+        }
+
+        const newItemList =[...itemsFromStorage,newItem]
+        window.localStorage.setItem('list', JSON.stringify(newItemList))
+
+        navigate("/")
+
+
+
+    }
+    const handleChange = event => {
+        setUnique(event.target.value);
+        unique === 'furniture' && setUniqueText('Dimensions')
+        unique === 'dvd' && setUniqueText('Size')
+        unique === 'book' && setUniqueText('Weight')
+    }
     return (
         <>
             <Link to="/">Cancel</Link>
-            <button>save</button>
+            <button onClick={handleSave}>save</button>
             <form>
                 <label>
                     SKU
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        onChange={event => {setSKU(event.target.value)}}
+                    />
                 </label>
 
                 <label>
                     Name
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        onChange={event => {setName(event.target.value)}}
+                    />
                 </label>
 
                 <label>
                     Price
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        onChange={event => {setPrice(event.target.value)}}
+                    />
                 </label>
 
                 <label>
-                    Name
-                    <input type="text"/>
+                    Type Switcher
+                    <select onChange={handleChange}>
+                        <option value="furniture">Furniture</option>
+                        <option value="dvd">DVD</option>
+                        <option value="book">Book</option>
+                    </select>
                 </label>
 
             </form>
