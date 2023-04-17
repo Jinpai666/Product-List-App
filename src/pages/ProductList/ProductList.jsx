d76dcf21af44a02e9a1ac8dbaf23d5d958b8b346
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-04-17 16:08:06 +0200

    fix error handling

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,49 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import './ProductList.scss'
+import React, { useEffect, useState } from "react";
+import "./ProductList.scss";
 import { Link } from "react-router-dom";
 import ListItem from "../../components/ListItem.jsx";
 import { getData, deleteData } from "../../utils/api.js";
 
-
 function ProductList() {
-
-    const [formData, setFormData] = useState([]);
+    const [items, setItems] = useState([]);
     const [selectedSkus, setSelectedSkus] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        console.log('loading')
-        getData(setFormData).then(() => setIsLoading(false));
+        getData()
+            .then((data) => {
+                setItems(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setError("Failed to load products. Please try again later.");
+                setIsLoading(false);
+            });
+    }, []);
 
-    }, [isLoading])
-
-    const handleDelete = () => {
-        setIsLoading(true)
-        const itemsToDelete = formData.filter(item => selectedSkus.includes(item.sku))
-        itemsToDelete.forEach(
-            item => deleteData(item.id, setIsLoading)
+    const handleDelete = async () => {
+        setIsLoading(true);
+        const itemsToDelete = items.filter((item) =>
+            selectedSkus.includes(item.sku)
         );
-    }
+        try {
+
+            await Promise.all(
+                itemsToDelete.map((item) => deleteData(item.id))
+            );
+            setItems((prevItems) =>
+                prevItems.filter((item) => !itemsToDelete.includes(item))
+            );
+            setSelectedSkus([]);
+            setError("");
+
+        } catch (error) {
+            setError("Failed to delete products. Please try again later.");
+        }
+        setIsLoading(false);
+    };
 
     return (
         <>
             <div>
-                <h2>test : {isLoading ? 'loading' : 'loaded'}</h2>
+                <h2>test: {isLoading ? "loading" : "loaded"}</h2>
                 <h1>Product List</h1>
                 <div>
                     <Link to="/add-product">ADD</Link>
-                    <button onClick={handleDelete}  className="delete-checkbox">MASS DELETE</button>
+                    <button onClick={handleDelete} className="delete-checkbox">
+                        MASS DELETE
+                    </button>
                 </div>
-                {formData.length > 0 ? (
+                {error && (
+                <p style={{color:'red'}}>{error}</p>
+                ) }
+                { items.length > 0 ? (
                     <div>
-                        {formData?.map((item) =>
+                        {items.map((item) => (
                             <ListItem
                                 key={item.sku}
                                 item={item}
                                 selectedSkus={selectedSkus}
                                 setSelectedSkus={setSelectedSkus}
                             />
-                        )}
+                        ))}
                     </div>
                 ) : (
                     <p>No products to display.</p>
@@ -52,5 +76,4 @@ function ProductList() {
         </>
     );
 }
-
-export default ProductList;
+export default ProductList
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,45 +1,36 @@
 import axios from "axios";
 
-export const getData = async (setItems) => {
+export const getData = async () => {
     try {
         const { data } = await axios.get(
-            'https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list.json'
+            "https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list.json"
         );
-        if (!data) {
-            console.log('no products');
-            setItems([]);
-            return;
-        }
-        const items = Object.keys(data).map((itemId) => ({
+        return data ? Object.keys(data).map((itemId) => ({
             id: itemId,
             ...data[itemId],
-        }));
-        setItems(items);
+        })) : [];
     } catch (error) {
-        setItems([]);
-        console.log(error);
+        throw error;
     }
-}
-
+};
 
 export const sendData = async (list) => {
     try {
         await axios.post(
-            'https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list.json',
-            list,
-        )
+            "https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list.json",
+            list
+        );
     } catch (error) {
-        console.log(error)
+        throw error;
     }
-}
+};
 
-export const deleteData = async (item, setReload) => {
+export const deleteData = async (itemId) => {
     try {
         await axios.delete(
-            `https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list/${item}.json`
+            `https://product-list-b59c8-default-rtdb.europe-west1.firebasedatabase.app/list/${itemId}.json`
         );
-        setReload(false);
     } catch (error) {
-        console.log(error);
+        throw error;
     }
-}
+};
